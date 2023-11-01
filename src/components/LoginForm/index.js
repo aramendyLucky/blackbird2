6fd2ba592e4925e84a5e3a051d8a5b26a0f04248
Lucas Aramendy <yataylucas@gmail.com>
2023-11-01 13:03:46 -0300

    Agrega la funcionalidad de validación de email y contraseña

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,62 +1,69 @@
-import { useState } from 'react';
-import Alert from '@mui/material/Alert';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Snackbar from '@mui/material/Snackbar';
-import Typography from '@mui/material/Typography';
-import logo from '../../assets/logo.svg';
-
+import { useState } from "react";
+import Alert from "@mui/material/Alert";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Snackbar from "@mui/material/Snackbar";
+import Typography from "@mui/material/Typography";
+import logo from "../../assets/logo.svg";
+import emailValidator from "email-validator"; // Importa la biblioteca de validación de correo electrónico
 
 export default function LoginForm() {
   const [showAlert, setShowAlert] = useState(false);
-  const validateForm = (event) => {
-    event.preventDefault()
-    const data = new FormData(event.currentTarget);
-    const email = data.get('email');
-    const password = data.get('password');
+  const [error, setError] = useState(false);
 
-    // Add validation code here
+  // Función de validación para validar el formulario
+  const validateForm = (email, password) => {
+    const isEmailValid = emailValidator.validate(email); // Valida el correo electrónico
+    const isPasswordValid =
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/.test(password); // Valida la contraseña según los requisitos específicos
+    return isEmailValid && isPasswordValid; // Devuelve true si tanto el correo electrónico como la contraseña son válidos
+  };
 
-  }
-
+  // Función para manejar el envío del formulario
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-    validateForm(event);
-    setShowAlert("Login Successful");
+    const email = data.get("email");
+    const password = data.get("password");
+    const isFormValid = validateForm(email, password);
+
+    if (isFormValid) {
+      setShowAlert("Login Successful"); // Muestra un mensaje de éxito si el formulario es válido
+      setError(false);
+    } else {
+      setShowAlert("Login Failed"); // Muestra un mensaje de error si el formulario no es válido
+      setError(true);
+    }
   };
 
   return (
     <>
-      {showAlert &&
+      {showAlert && (
         <Snackbar
           open={showAlert}
           autoHideDuration={6000}
           onClose={() => setShowAlert(false)}
-          message={showAlert}
         >
-          <Alert>{showAlert}</Alert>
+          <Alert severity={error ? "error" : "success"}>{showAlert}</Alert>
         </Snackbar>
-      }
+      )}
       <Grid
         item
         xs={false}
         sm={4}
         md={7}
         sx={{
-          backgroundImage: 'url(https://source.unsplash.com/random)',
-          backgroundRepeat: 'no-repeat',
+          backgroundImage: "url(https://source.unsplash.com/random)",
+          backgroundRepeat: "no-repeat",
           backgroundColor: (t) =>
-            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
+            t.palette.mode === "light"
+              ? t.palette.grey[50]
+              : t.palette.grey[900],
+          backgroundSize: "cover",
+          backgroundPosition: "center",
         }}
       />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
@@ -64,20 +71,27 @@ export default function LoginForm() {
           sx={{
             my: 8,
             mx: 4,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Box sx={{
-            my: 2
-          }}>
+          <Box
+            sx={{
+              my: 2,
+            }}
+          >
             <img src={logo} width="147" alt="harrison.ai" />
           </Box>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 1 }}
+          >
             <TextField
               margin="normal"
               required
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+};
